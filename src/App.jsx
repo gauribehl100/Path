52e68b233bb9e87537ed2258166973c3dcda5e28
gauribehl100 <gauribehl02@gmail.com>
2025-01-27 03:22:32 +0530

    Path

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pathfinding from './pathfind/Pathfinding';
+
+function App() {
+  return (
+    <div className="App">
+    <Pathfinding/>
+    </div>
+  );
+}
+
+export default App;
