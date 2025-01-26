44098b133bbf8a231bdff6110cdf151b3c6360e1
gauribehl100 <140520216+gauribehl100@users.noreply.github.com>
2025-01-27 03:59:44 +0530

    Pathfinding.jsx

--- src/pathfind/Pathfinding.jsx
+++ src/pathfind/Pathfinding.jsx
@@ -395,19 +395,6 @@ export default class Pathfinding extends Component {
           <a className="navbar-brand" href="/">
             <b>PathFinding Visualizer</b>
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-            </ul>
           </div>
         </nav>
 
